5e6564306ef0ee3729bb423d6ff259365fcd5e9b
gopalbbharadva <gopalbbharadva@gmail.com>
2021-05-17 19:46:28 +0530

    feat:expense item creation

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
-const btn=document.querySelector("button");
-console.log(btn);
+const btn = document.querySelector("button");
+const result = document.querySelector("h3");
+const expenseAmt = document.querySelector("#expenseAmount");
+const expenseDesc = document.querySelector("#expenseDesc");
+
+let expenseAmount = 0,
+  expenseDescription = null;
+totalAmount = 0;
+let expenseAllItems = [];
+const inputHandler = () => {
+  expenseAmt.value ? (btn.disabled = false) : (btn.disabled = true);
+};
+const btnHandler = () => {
+  let currentItem = {};
+  expenseAmount = expenseAmt.value;
+  expenseDescription = expenseDesc.value;
+  totalAmount += parseInt(expenseAmount);
+  result.textContent = `Total:${totalAmount}`;
+  currentItem.item = expenseAmount;
+  currentItem.itemDesc = expenseDescription;
+  expenseAllItems.push(currentItem);
+  console.table(expenseAllItems);
+  expenseAmt.value = "";
+  expenseDesc.value = "";
+};
+btn.addEventListener("click", btnHandler);
+expenseAmt.addEventListener("input", inputHandler);
