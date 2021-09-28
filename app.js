286abb88457e787a3e7d3d7a3865712c2282f6bd
gopalbbharadva <gopalbbharadva@gmail.com>
2021-09-28 21:46:13 +0530

    feat: delete an expense

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const expenseAmt = document.querySelector("#expenseAmount");
 const expenseDesc = document.querySelector("#expenseDesc");
 const output = document.querySelector(".output");
 const form = document.querySelector(".inputDiv");
+let currentItem = {};
 
 let expenseAmount = 0,
   expenseDescription = null;
@@ -25,7 +26,6 @@ const inputHandler = () => {
 };
 const btnHandler = (e) => {
   e.preventDefault();
-  let currentItem = {};
   expenseAmount = expenseAmt.value;
   expenseDescription = expenseDesc.value;
 
@@ -43,29 +43,26 @@ const btnHandler = (e) => {
   inputHandler();
 };
 function getDate(date) {
-  let time=`${date.toDate().getHours()}:${date.toDate().getMinutes()}`
-  return time
-  
+  let time = `${date.toDate().getHours()}:${date.toDate().getMinutes()}`;
+  return time;
 }
 
 function addItemDb(itemObj) {
   let d = new Date();
-  let str=d.getTime();
-  return firebase.firestore().collection("expenses").add({
-    id:str,
+  let str = d.getTime();
+  firebase.firestore().collection("expenses").add({
+    id: str,
     itemAmount: itemObj.itemAmt,
     description: itemObj.itemDesc,
     createdAt: d,
   });
-  //
 }
-
+let mainDiv;
 function createItem(itemObj) {
-  let id = itemObj.doc.data().createdAt;
-  // console.log(id);
-  const mainDiv = document.createElement("div");
-  mainDiv.innerHTML = `<div class="expenseItem">
-  <div style="display:flex;flex-direction:column;align-items:flex-start">
+  let id = itemObj.doc.data().id;
+  mainDiv = document.createElement("div");
+  mainDiv.innerHTML = `<div id="${id}" class="expenseItem">
+  <div  style="display:flex;flex-direction:column;align-items:flex-start">
   <p>${itemObj.doc.data().description}</p>
   <small>${getDate(itemObj.doc.data().createdAt)}</small>
   </div>
@@ -82,20 +79,36 @@ function createItem(itemObj) {
 }
 getData();
 function getData() {
-  console.log("get data function");
   firebase
     .firestore()
     .collection("expenses")
     .orderBy("createdAt")
     .onSnapshot((snapshot) => {
-      snapshot.docChanges().forEach((item) => {
-        if (item.type === "added") createItem(item);
+      let changes = snapshot.docChanges();
+      // console.log(changes[0].doc.data().description);
+      changes.forEach((item) => {
+        if (item.type == "added") {
+          createItem(item);
+        } else if (item.type == "removed") {
+          let div = document.getElementById(item.doc.data().id);
+          div.remove();
+        }
       });
     });
 }
 
-function deleteItem(itemId) {
+function deleteItem(itemId, itemDate) {
   console.log(itemId);
+  console.log(itemDate);
+  let query = firebase
+    .firestore()
+    .collection("expenses")
+    .where("id", "==", itemId);
+  query.get().then((snapshot) => {
+    snapshot.forEach((doc) => {
+      doc.ref.delete();
+    });
+  });
 }
 form.addEventListener("submit", (e) => btnHandler(e));
 expenseAmt.addEventListener("input", inputHandler);
